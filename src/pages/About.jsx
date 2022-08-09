955c4057aabd5c4aa91d0c44a50e3921cb59a4bb
Stíofán ♔ Ó Conchubhair <stevo.joc@gmail.com>
2022-08-09 16:01:15 +0100

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,13 +5,15 @@ const HomePage = () => {
   return (
     <>
       <Navbar />
-      <header>
-        <h1>A React Redux boilerplate</h1>
-      </header>
-      <main>
-        <h2>This will be my boilerplate for React projects</h2>
-        <p></p>
-      </main>
+      <div className="App">
+        <header>
+          <h1>A React Redux boilerplate</h1>
+        </header>
+        <main>
+          <h2>This will be my boilerplate for React projects</h2>
+          <p></p>
+        </main>
+      </div>
     </>
   );
 };
